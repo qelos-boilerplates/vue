cdb51cd6ccd2ba37f17c5ca7111ff0a56269d076
David Meir-Levy <david@davidlevy.co.il>
2024-04-17 20:41:54 +0300

    fix build

--- app-ui/vite.config.ts
+++ app-ui/vite.config.ts
@@ -6,6 +6,7 @@ import mkcert from "vite-plugin-mkcert";
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import { fileURLToPath } from 'node:url';
 
 const serverHasCert = fs.existsSync(fileURLToPath(new URL('../cert/ca.pem', import.meta.url)))
 
